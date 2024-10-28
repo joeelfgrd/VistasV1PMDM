98028f0f9906feeb4b8ddf33dfe434951df020e6
joeelfgrd <joelfigueiridomolares@gmail.com>
2024-10-28 10:35:43 +0100

    Ejercicio acabado

--- app/src/main/java/com/example/vistasv1ej1/MainActivity.java
+++ app/src/main/java/com/example/vistasv1ej1/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.vistasv1ej1;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView tvRespuesta;
     Button btnClick;
+    Button btnFinalizar;
+    Button btnReiniciar;
     int contador = 0;
 
 
@@ -22,17 +18,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         tvRespuesta = findViewById(R.id.tvRespuesta);
         btnClick = findViewById(R.id.btnClick);
-        btnClick.setOnClickListener(this);
-    }
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+        btnReiniciar = findViewById(R.id.btnReiniciar);
 
+        btnClick.setOnClickListener(this);
+        btnFinalizar.setOnClickListener(escuchadorFinalizar);
+        btnReiniciar.setOnClickListener(escuchadorReiniciar);
+    }
 
     @Override
     public void onClick(View view) {
         contador++;
-        tvRespuesta.setText("Has pulsado el botón " + contador + " veces");
     }
+
+    private View.OnClickListener escuchadorFinalizar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            tvRespuesta.setText(getResources().getString(R.string.has_pulsado) + contador + getResources().getString(R.string.veces));
+        }
+    };
+    private View.OnClickListener escuchadorReiniciar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            contador = 0;
+        }
+    };
+
 
 
 }
