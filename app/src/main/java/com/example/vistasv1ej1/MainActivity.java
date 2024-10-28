07cb0a64280483f9d805f9ea240140d19173420d
joeelfgrd <joelfigueiridomolares@gmail.com>
2024-10-28 13:36:20 +0100

    Ej5 a medias 2

--- app/src/main/java/com/example/vistasv1ej1/MainActivity.java
+++ app/src/main/java/com/example/vistasv1ej1/MainActivity.java
@@ -1,37 +1,53 @@
 package com.example.vistasv1ej1;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
-    TextView tvRespuesta;
+
+    RadioButton rbTexto;
+    RadioButton rbImagen;
+    Button btnAceptar;
+    TextView tvSalidaPantalla;
+    float rotacion = 0;
 
 
-
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rbTexto = findViewById(R.id.rbTexto);
+        rbImagen = findViewById(R.id.rbImagen);
+        btnAceptar = findViewById(R.id.btAceptar);
+        tvSalidaPantalla = findViewById(R.id.tvSalidaPantalla);
+        btnAceptar.setOnClickListener(escuchadorAceptar);
 
-        tvRespuesta = findViewById(R.id.tvRespuesta);
-        tvRespuesta.setOnClickListener(escuchadorRespuesta);
+
+
+
     }
 
 
-    private View.OnClickListener escuchadorRespuesta = new View.OnClickListener() {
+
+
+    private View.OnClickListener escuchadorAceptar = new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
-            float rotacion = tvRespuesta.getRotation();
-            tvRespuesta.setRotation(rotacion + 45);
-            tvRespuesta.setText("HOLAAAAAAAAAA!!!");
-            tvRespuesta.setBackgroundColor(getResources().getColor(R.color.green));
+        public void onClick(View v) {
+
+            if (rbTexto.isChecked()) {
+                setContentView(R.layout.ventana_saludo);
+                TextView tvSaludo = findViewById(R.id.tvSalidaPantalla);
+                if (tvSaludo != null) {
+                    rotacion += 45;
+                    tvSaludo.setRotation(rotacion);
+                }
+            }
         }
     };
-
-
-
 }
