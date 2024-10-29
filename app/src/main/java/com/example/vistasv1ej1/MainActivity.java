43098430d80f381f646183ae5f49ade34381d427
joeelfgrd <joelfigueiridomolares@gmail.com>
2024-10-29 12:47:11 +0100

    EJERCICIO 5 ACABADO

--- app/src/main/java/com/example/vistasv1ej1/MainActivity.java
+++ app/src/main/java/com/example/vistasv1ej1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.vistasv1ej1;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rbImagen;
     Button btnAceptar;
     TextView tvSalidaPantalla;
-    float rotacion = 0;
+    ImageView ivImagen;
+    float rotacion = 45;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         rbTexto = findViewById(R.id.rbTexto);
         rbImagen = findViewById(R.id.rbImagen);
         btnAceptar = findViewById(R.id.btAceptar);
+        ivImagen = findViewById(R.id.ivImagen);
         tvSalidaPantalla = findViewById(R.id.tvSalidaPantalla);
 
         btnAceptar.setOnClickListener(escuchadorAceptar);
@@ -35,18 +41,34 @@ public class MainActivity extends AppCompatActivity {
             if (rbTexto.isChecked()) {
                 setContentView(R.layout.ventana_saludo);
                 tvSalidaPantalla = findViewById(R.id.tvSalidaPantalla);
-
-                if (tvSalidaPantalla != null) {
-                    rotacion += 45;
-                    tvSalidaPantalla.setRotation(rotacion);
-                    tvSalidaPantalla.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            rotacion += 45;
-                            tvSalidaPantalla.setRotation(rotacion);
+                rotacion = 45;
+                tvSalidaPantalla.setRotation(rotacion);
+                tvSalidaPantalla.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tvSalidaPantalla.setRotation(rotacion);
+                        if (rotacion == 360){
+                            rotacion = 45;
                         }
-                    });
-                }
+                        rotacion += 45;
+
+                    }
+                });
+            } else if (rbImagen.isChecked()) {
+                setContentView(R.layout.ventana_imagen);
+                ivImagen = findViewById(R.id.ivImagen);
+                rotacion = 45;
+                ivImagen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ivImagen.setRotation(rotacion);
+                        if (rotacion == 360){
+                            rotacion = 45;
+                        }
+                        rotacion += 45;
+
+                    }
+                });
             }
         }
     };
